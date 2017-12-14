0d4ca20f4323a46d0c8232c1eb0cecbfd33250c6
GabrielRM <gabrielrm@MacBook-Air-de-Gabriel.local>
2017-12-14 17:54:43 -0200

    Novas atualizações

---
+++ pollos.sql
@@ -0,0 +1,29 @@
+DROP SCHEMA IF EXISTS pollos;
+CREATE SCHEMA IF NOT EXISTS pollos;
+USE pollos;
+-- Pessoas
+DROP TABLE IF EXISTS pessoas;
+CREATE TABLE IF NOT EXISTS pessoas ( 
+	id INT NOT NULL AUTO_INCREMENT , 
+	cpf TEXT NOT NULL , 
+	nome TEXT NOT NULL , 
+	idade INT NOT NULL , 
+	email TEXT NOT NULL , 
+	funcionario INT NOT NULL ,  
+	telefone TEXT NOT NULL , 
+	endereco TEXT NOT NULL , 
+	observacoes TEXT NOT NULL , 
+	PRIMARY KEY (id)
+);
+-- Pedidos
+DROP TABLE IF EXISTS pedidos;
+CREATE TABLE IF NOT EXISTS pedidos ( 
+	id INT NOT NULL AUTO_INCREMENT , 
+	codCliente TEXT NOT NULL , 
+	codProduto TEXT NOT NULL , 
+	quantidade INT NOT NULL , 
+	status TEXT NOT NULL , 
+	observacoes TEXT NOT NULL , 
+	valor DOUBLE NOT NULL ,  
+	PRIMARY KEY (id)
+);
